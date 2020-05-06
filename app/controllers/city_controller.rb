91cf7e00080866d88015be1cfc043fbb7eec70f7
Mathildedej <mathilde.dejaureguiberry@gmail.com>
2020-05-06 18:27:25 +0200

    City show working

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -1,4 +1,13 @@
 class CityController < ApplicationController
   def show
+    @city = City.find(params[:id])
+    @gossips = []
+    City.all.each do |city|
+      city.users.where(city_id: params[:id]).each do |user|
+        user.gossips.each do |gossip|
+          @gossips << gossip
+        end
+      end
+    end
   end
 end
